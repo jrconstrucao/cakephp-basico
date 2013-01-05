5036c86c267b9a24446a8b5cec8e73ac0627f7a4
jrconstrucao <jr.faleconosco@gmail.com>
2013-01-05 00:43:13 -0200

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,3 +1,3 @@
-//adicionei estas linhas  
+//adicionei estas linhas, para chamar o controller pages, poderá ser chamado por dois modos : /pagina/ ou /  
   Router::connect('/pagina/*', array('controller' => 'pages', 'action' => 'display'));
   Router::connect('/*', array('controller' => 'pages', 'action' => 'display'));
