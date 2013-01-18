c40e9d36063b5fd76e350d858a68465d6621be7e
jrconstrucao <jr.faleconosco@gmail.com>
2013-01-17 22:47:46 -0200

    Update Inscricao.php

--- Inscricao.php
+++ Inscricao.php
@@ -5,21 +5,26 @@ class Inscricao extends AppModel {
     public $order = array('nome' => 'ASC');
 
     public $validate = array(
-    	'nome' => array(
+  		'nome' => array(
   			'rule' => 'notEmpty',
         'message'=>'Campo vazio'	
   			),
   		'email' => array(			
-  			'rule' => 'notEmpty',
-        'message'=>'Campo vazio'
+  			'rule' => 'email',
+        'message'=>'Email incorreto'
   			),
-      'endereco' => array(			
-  			'rule' => 'notEmpty',
-        'message'=>'Campo vazio'
-  			),
+      'endereco' => array(
+            	'notempty' => array( 
+                            'rule' => array('minLength', '10'), 
+                           	'required' => true, 
+                           	'allowEmpty' => false, 
+                           	'message' => 'Endereco deve ter no minimo 10 caracteres' 
+              ) 
+        ),
       'telefone' => array(			
   			'rule' => 'notEmpty',
         'message'=>'Campo vazio'
   			)  
 		);
 }
+?>
